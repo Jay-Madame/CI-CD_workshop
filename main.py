eefa1e68c522753fefc1e88baffbc80f861a7284
Jay <jay@Jewells-MacBook-Air.local>
2026-02-26 12:27:38 -0600

    ADD: Files

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+
+def fix_me(num1, num2):
+    return num1 + num2 + 3
+
+# Yes this is overly cutesy. I love cinnamaroll man
+
+print("\n◦∘●∘◦❀◦∘●∘◦❀◦∘●∘◦❀◦∘●∘◦❀◦∘●∘◦")
+print("--! Running calculations !--\n")
+
+num1 = random.randint(1, 100)
+num2 = random.randint(1, 100)
+
+print(f"Adding {num1} and {num2} together to get...")
+print(fix_me(num1, num2))
